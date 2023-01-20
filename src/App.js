e03efe9080d9a293aca1af837ca0bc89b24b3063
Arpit Singh <arpitrahul02@gmail.com>
2023-01-20 12:37:49 +0530

    create the wheel menu

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className = "wheelContainer">
+      <div className = "wheelBackground">
+        <div className = "wheelMenu">
+          <div className = "wheelForeground">
+          </div>
+          <span className='menuSpan'> Menu </span>
+          <img  className='forward' alt = "" src = "https://cdn-icons-png.flaticon.com/512/4211/4211386.png" />
+          <img  className='backward' alt = "" src = "https://cdn-icons-png.flaticon.com/512/3318/3318703.png" />
+          <img  className='playPause' alt = "" src = "https://cdn-icons-png.flaticon.com/512/7960/7960808.png" />
+          
+        </div>
+      </div>
+     </div>
+    </div>
+  );
+}
+
+export default App;

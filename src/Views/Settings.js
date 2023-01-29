60b8d15880d76e89ee2a5f7002b84380295b5cd2
Arpit Singh <arpitrahul02@gmail.com>
2023-01-29 13:08:17 +0530

    edit games and settings

--- src/Views/Settings.js
+++ src/Views/Settings.js
@@ -1,20 +1,11 @@
+import '../Static/Settings.css';
+
 function Settings(){
     return(
-        <div style={styles.settings}>
-            <img style={styles.dim} alt = "" src = "https://play-lh.googleusercontent.com/mOMVlWK_cLLdOOO3XUWiw1zFf7FO15f_ys2XmcYKJLOMTHF-eGtQddXE-wYDSd9kd5Q" />
+        <div className="settings">
+            {/* <img style={styles.dim} alt = "" src = "https://play-lh.googleusercontent.com/mOMVlWK_cLLdOOO3XUWiw1zFf7FO15f_ys2XmcYKJLOMTHF-eGtQddXE-wYDSd9kd5Q" /> */}
         </div>
     )
-}
-
-const styles = {
-    settings: {
-        height: '100%',
-        width: '100%'
-    },
-    dim: {
-        height: '100%',
-        width: '100%'
-    }
 }
 
 export default Settings
